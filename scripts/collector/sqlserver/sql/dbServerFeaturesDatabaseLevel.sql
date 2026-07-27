d4cddb16ca6220c7e0989d09772168c0d36d3df2
Tim Chapman <chapmantim@google.com>
2026-07-27 13:52:09 +0000

    revert: restore dbServerFeaturesDatabaseLevel.sql, dbSizes.sql, and linkedServersDetail.sql to main version

--- scripts/collector/sqlserver/sql/dbServerFeaturesDatabaseLevel.sql
+++ scripts/collector/sqlserver/sql/dbServerFeaturesDatabaseLevel.sql
@@ -1,16 +1,39 @@
 /*
-Copyright 2026 Google LLC
+Copyright 2023 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 */
+
 SET NOCOUNT ON;
 SET LANGUAGE us_english;
 
 DECLARE @PKEY AS VARCHAR(256)
+DECLARE @CLOUDTYPE AS VARCHAR(256)
+DECLARE @PRODUCT_VERSION AS INTEGER
+DECLARE @TABLE_PERMISSION_COUNT AS INTEGER
+DECLARE @ROW_COUNT_VAR AS INTEGER
 DECLARE @DMA_SOURCE_ID AS VARCHAR(256)
 DECLARE @DMA_MANUAL_ID AS VARCHAR(256)
 
 SELECT @PKEY = N'$(pkey)';
+SELECT @CLOUDTYPE = 'NONE';
+SELECT @PRODUCT_VERSION = CONVERT(INTEGER, PARSENAME(CONVERT(NVARCHAR(255), SERVERPROPERTY('productversion')), 4));
 SELECT @DMA_SOURCE_ID = N'$(dmaSourceId)';
 SELECT @DMA_MANUAL_ID = N'$(dmaManualId)';
+
+IF UPPER(@@VERSION) LIKE '%AZURE%'
+	SELECT @CLOUDTYPE = 'AZURE'
 
 IF OBJECT_ID('tempdb..#FeaturesEnabledDbLevel') IS NOT NULL
    DROP TABLE #FeaturesEnabledDbLevel;
@@ -18,131 +41,104 @@ IF OBJECT_ID('tempdb..#FeaturesEnabledDbLevel') IS NOT NULL
 CREATE TABLE #FeaturesEnabledDbLevel
 (
     database_name nvarchar(255) DEFAULT db_name(),
-    feature_name NVARCHAR(255),
+    feature_name NVARCHAR(40),
     is_enabled_or_used NVARCHAR(1),
     occurance_count INT
 );
 
--- 1. Row Level Security Policies
+--Security Policies
 BEGIN TRY
     exec('INSERT INTO #FeaturesEnabledDbLevel
             SELECT
                 db_name(),
-                ''Row Level Security Policies (RLS)'',
-                CASE WHEN count(*) > 0 THEN ''1'' ELSE ''0'' END,
+                ''SP'',
+                CASE
+                    WHEN count(*) > 0 THEN ''1''
+                    ELSE ''0''
+                END,
                 CONVERT(int, count(*))
             FROM sys.security_policies
-            WHERE is_enabled = 1');
+            WHERE is_enabled = 1') /* SQL Server 2016 (13.x) and above */ ;
 END TRY
 BEGIN CATCH
-    INSERT INTO #FeaturesEnabledDbLevel VALUES (db_name(), 'Row Level Security Policies (RLS)', '0', 0);
-END CATCH;
+    IF ERROR_NUMBER() = 208 AND ERROR_SEVERITY() = 16 AND ERROR_STATE() = 1
+    BEGIN
+    exec('INSERT INTO #FeaturesEnabledDbLevel SELECT db_name(), ''SP'', ''0'', 0')
+/* SQL Server 2014 (12.x) and below */
+;
+END
+    ELSE
+    BEGIN
+    exec('INSERT INTO #FeaturesEnabledDbLevel SELECT db_name(), ''SP'', ''0'', 0')
+/* SQL Server 2014 (12.x) and below */
+;
+END
+END CATCH
 
--- 2. FILESTREAM File Tables
+--File Tables Detected
 BEGIN TRY
     exec('INSERT INTO #FeaturesEnabledDbLevel
             SELECT
                 db_name(),
-                ''FILESTREAM File Tables'',
-                CASE WHEN count(*) > 0 THEN ''1'' ELSE ''0'' END,
+                ''IsFileTablesEnabled'',
+                CASE
+                    WHEN count(*) > 0 THEN ''1''
+                    ELSE ''0''
+                END,
                 CONVERT(int, count(*))
             FROM sys.filetables
-            WHERE is_enabled = 1');
+            WHERE is_enabled = 1') /* SQL Server 2016 (13.x) and above */ ;
 END TRY
 BEGIN CATCH
-    INSERT INTO #FeaturesEnabledDbLevel VALUES (db_name(), 'FILESTREAM File Tables', '0', 0);
-END CATCH;
+    IF ERROR_NUMBER() = 208 AND ERROR_SEVERITY() = 16 AND ERROR_STATE() = 1
+    BEGIN
+    exec('INSERT INTO #FeaturesEnabledDbLevel SELECT db_name(), ''IsFileTablesEnabled'', ''0'', 0')
+/* SQL Server 2014 (12.x) and below */
+;
+END
+    ELSE
+    BEGIN
+    exec('INSERT INTO #FeaturesEnabledDbLevel SELECT db_name(), ''IsFileTablesEnabled'', ''0'', 0')
+/* SQL Server 2014 (12.x) and below */
+;
+END
+END CATCH
 
--- 3. In-Memory OLTP Tables
-BEGIN TRY
-    exec('INSERT INTO #FeaturesEnabledDbLevel
-            SELECT
-                db_name(),
-                ''In-Memory OLTP Tables'',
-                CASE WHEN count(*) > 0 THEN ''1'' ELSE ''0'' END,
-                CONVERT(int, count(*))
-            FROM sys.tables
-            WHERE is_memory_optimized = 1');
-END TRY
-BEGIN CATCH
-    INSERT INTO #FeaturesEnabledDbLevel VALUES (db_name(), 'In-Memory OLTP Tables', '0', 0);
-END CATCH;
+/* Collect permissions which are unsupported in CloudSQL SQL Server */
+BEGIN
+    BEGIN TRY
+            exec('INSERT INTO #FeaturesEnabledDbLevel
+                SELECT
+                    db_name(),
+                    tmp.permission_name,
+					CASE WHEN count(1) > 0 THEN 1 ELSE 0 END,
+                    count(1)
+                FROM (
+                    SELECT
+                        pr.name,
+                        pr.type,
+                        pr.type_desc,
+                        dp.permission_name,
+                        dp.type AS permission_type
+                    FROM
+                        sys.database_permissions dp
+                        INNER JOIN sys.server_principals pr ON dp.grantee_principal_id = pr.principal_id
+                    WHERE
+                        pr.name NOT LIKE ''NT SERVICE\%''
+                        AND dp.permission_name IN (''ADMINISTER BULK OPERATIONS'', ''ALTER ANY CREDENTIAL'',
+                        ''ALTER ANY EVENT NOTIFICATION'', ''ALTER ANY EVENT SESSION'', ''ALTER RESOURCES'',
+                        ''ALTER SETTINGS'', ''AUTHENTICATE SERVER'', ''CONTROL SERVER'',
+                        ''CREATE DDL EVENT NOTIFICATION'', ''CREATE ENDPOINT'', ''CREATE TRACE EVENT NOTIFICATION'',
+                        ''EXTERNAL ACCESS ASSEMBLY'', ''SHUTDOWN'', ''EXTERNAL ASSEMBLIES'', ''CREATE ASSEMBLY'')) tmp
+                GROUP BY
+                    tmp.permission_name');
+    END TRY
+    BEGIN CATCH
+        IF ERROR_NUMBER() = 208 AND ERROR_SEVERITY() = 16 AND ERROR_STATE() = 1
+            WAITFOR DELAY '00:00:00'
+    END CATCH
+END
 
--- 4. Temporal Tables
-BEGIN TRY
-    exec('INSERT INTO #FeaturesEnabledDbLevel
-            SELECT
-                db_name(),
-                ''System-Versioned Temporal Tables'',
-                CASE WHEN count(*) > 0 THEN ''1'' ELSE ''0'' END,
-                CONVERT(int, count(*))
-            FROM sys.tables
-            WHERE temporal_type = 2');
-END TRY
-BEGIN CATCH
-    INSERT INTO #FeaturesEnabledDbLevel VALUES (db_name(), 'System-Versioned Temporal Tables', '0', 0);
-END CATCH;
-
--- 5. Change Data Capture (CDC) Tracked Tables
-BEGIN TRY
-    exec('INSERT INTO #FeaturesEnabledDbLevel
-            SELECT
-                db_name(),
-                ''Change Data Capture (CDC) Tracked Tables'',
-                CASE WHEN count(*) > 0 THEN ''1'' ELSE ''0'' END,
-                CONVERT(int, count(*))
-            FROM sys.tables
-            WHERE is_tracked_by_cdc = 1');
-END TRY
-BEGIN CATCH
-    INSERT INTO #FeaturesEnabledDbLevel VALUES (db_name(), 'Change Data Capture (CDC) Tracked Tables', '0', 0);
-END CATCH;
-
--- 6. Columnstore Indexes
-BEGIN TRY
-    exec('INSERT INTO #FeaturesEnabledDbLevel
-            SELECT
-                db_name(),
-                ''Columnstore Indexes (Clustered or Nonclustered)'',
-                CASE WHEN count(*) > 0 THEN ''1'' ELSE ''0'' END,
-                CONVERT(int, count(*))
-            FROM sys.indexes
-            WHERE type IN (5, 6)');
-END TRY
-BEGIN CATCH
-    INSERT INTO #FeaturesEnabledDbLevel VALUES (db_name(), 'Columnstore Indexes (Clustered or Nonclustered)', '0', 0);
-END CATCH;
-
--- 7. Dynamic Data Masking
-BEGIN TRY
-    exec('INSERT INTO #FeaturesEnabledDbLevel
-            SELECT
-                db_name(),
-                ''Dynamic Data Masking (Masked Columns)'',
-                CASE WHEN count(*) > 0 THEN ''1'' ELSE ''0'' END,
-                CONVERT(int, count(*))
-            FROM sys.masked_columns');
-END TRY
-BEGIN CATCH
-    INSERT INTO #FeaturesEnabledDbLevel VALUES (db_name(), 'Dynamic Data Masking (Masked Columns)', '0', 0);
-END CATCH;
-
--- 8. Service Broker Enabled
-BEGIN TRY
-    exec('INSERT INTO #FeaturesEnabledDbLevel
-            SELECT
-                db_name(),
-                ''Service Broker Enabled'',
-                CASE WHEN is_broker_enabled = 1 THEN ''1'' ELSE ''0'' END,
-                CONVERT(int, is_broker_enabled)
-            FROM sys.databases
-            WHERE name = db_name()');
-END TRY
-BEGIN CATCH
-    INSERT INTO #FeaturesEnabledDbLevel VALUES (db_name(), 'Service Broker Enabled', '0', 0);
-END CATCH;
-
--- Final Payload
 SELECT
     '"' + @PKEY + '"' as PKEY,
     '"' + CONVERT(NVARCHAR(MAX), f.database_name) + '"' as database_name,
--- scripts/collector/sqlserver/sql/linkedServersDetail.sql
+++ scripts/collector/sqlserver/sql/linkedServersDetail.sql
@@ -1,5 +1,5 @@
 /*
-Copyright 2026 Google LLC
+Copyright 2023 Google LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 */
+--Linked Servers
 SET NOCOUNT ON;
 SET LANGUAGE us_english;
 
@@ -32,49 +33,26 @@ SELECT @DMA_MANUAL_ID = N'$(dmaManualId)';
 IF UPPER(@@VERSION) LIKE '%AZURE%'
 	SELECT @CLOUDTYPE = 'AZURE'
 
-IF OBJECT_ID('tempdb..#LinkedServers') IS NOT NULL
-   DROP TABLE #LinkedServers;
-
-CREATE TABLE #LinkedServers (
-    name NVARCHAR(255),
-    product NVARCHAR(255),
-    provider NVARCHAR(255),
-    data_source NVARCHAR(MAX),
-    location NVARCHAR(255),
-    provider_string NVARCHAR(MAX),
-    catalog NVARCHAR(255)
-);
-
-BEGIN TRY
-    INSERT INTO #LinkedServers
-    SELECT
-        name, product, provider, data_source, location, provider_string, catalog
-    FROM sys.servers
-    WHERE is_linked = 1 AND server_id <> 0;
-END TRY
-BEGIN CATCH
-    WAITFOR DELAY '00:00:00';
-END CATCH;
-
-IF NOT EXISTS (SELECT 1 FROM #LinkedServers)
 BEGIN
-    INSERT INTO #LinkedServers VALUES (
-        'NONE_CONFIGURED', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A'
-    );
+    BEGIN TRY
+    exec('
+        select
+            ''"' + @PKEY + '"'' AS pkey,
+            ''"'' + CONVERT(NVARCHAR(MAX), name) + ''"'' as name,
+            ''"'' + CONVERT(NVARCHAR(MAX), product) + ''"'' as product,
+            ''"'' + CONVERT(NVARCHAR(MAX), provider) + ''"'' as provider,
+            ''"'' + CONVERT(NVARCHAR(MAX), data_source) + ''"'' as data_source,
+            ''"'' + CONVERT(NVARCHAR(MAX), location) + ''"'' as location,
+            ''"'' + CONVERT(NVARCHAR(MAX), provider_string) + ''"'' as provider_string,
+            ''"'' + CONVERT(NVARCHAR(MAX), catalog) + ''"'' as catalog,
+            ''"' + @DMA_SOURCE_ID + '"'' as dma_source_id,
+            ''"' + @DMA_MANUAL_ID + '"'' as dma_manual_id
+        from sys.servers
+        where is_linked = 1
+            and server_id <> 0');
+    END TRY
+    BEGIN CATCH
+        IF ERROR_NUMBER() = 208 AND ERROR_SEVERITY() = 16 AND ERROR_STATE() = 1
+            WAITFOR DELAY '00:00:00'
+    END CATCH
 END;
-
-SELECT
-    '"' + @PKEY + '"' AS pkey,
-    '"' + CONVERT(NVARCHAR(MAX), name) + '"' as name,
-    '"' + CONVERT(NVARCHAR(MAX), product) + '"' as product,
-    '"' + CONVERT(NVARCHAR(MAX), provider) + '"' as provider,
-    '"' + CONVERT(NVARCHAR(MAX), data_source) + '"' as data_source,
-    '"' + CONVERT(NVARCHAR(MAX), location) + '"' as location,
-    '"' + CONVERT(NVARCHAR(MAX), provider_string) + '"' as provider_string,
-    '"' + CONVERT(NVARCHAR(MAX), catalog) + '"' as catalog,
-    '"' + @DMA_SOURCE_ID + '"' as dma_source_id,
-    '"' + @DMA_MANUAL_ID + '"' as dma_manual_id
-FROM #LinkedServers;
-
-IF OBJECT_ID('tempdb..#LinkedServers') IS NOT NULL
-   DROP TABLE #LinkedServers;
